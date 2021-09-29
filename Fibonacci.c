523e83d3cfb1433aecafcdc56eda7f35483cf912
HugoCQ <hugo.conchaq@alumnos.ucentral.cl>
2021-09-29 19:26:43 -0300

    Alpha 1.5

--- Fibonacci.c
+++ Fibonacci.c
@@ -8,16 +8,31 @@ int fibonacci(int *inicial,int *Seg_inicial,int *Generador)
     *Generador= *inicial + *Seg_inicial;
     return *Generador;
 }
-
-int main()
+int ciclos(int *a,int *b,int *c,int *d )
 {
+    int anchoDias = -15;
+    int anchoPCs = -15;
+    printf("+---------------+---------------+\n");
+    printf("|Computadoras infectadas por dia|\n");
+    printf("+---------------+---------------+\n");
+    printf("|%*s|%*s|\n", anchoDias, " Dia", anchoPCs,
+    " PC");
+    printf("+---------------+---------------+\n");
+    for (int i=0;i<*a;i++)
+    {
+        printf("| Dia: %-9i| %-14d|\n",i+1,fibonacci(b,c,d));
+        
+    }
+    printf("+---------------+---------------+\n");
+}
+
+int main(void)
+{
+    system("cls");
     int n,inicial=0, Seg_inicial=1, Generador=0;
 
     printf("Ingrese el numero de computadores infectador por dias:");
     scanf("%i",&n);
-    for (int i=0;i<n;i++)
-    {
-        printf("%d\n",fibonacci(&inicial,&Seg_inicial,&Generador));
-    }
+    ciclos(&n,&inicial,&Seg_inicial,&Generador);
     return 0;
 }
